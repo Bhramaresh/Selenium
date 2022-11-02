a47c575d2f8f5b97c9641e8d42a81c1aaac9770f
bhramaresh <bhramareshballa9646@gmail.com>
2022-11-02 19:08:10 +0530

    changes

--- src/main/java/com/Selenium/Practice/ExplicitWait.java
+++ src/main/java/com/Selenium/Practice/ExplicitWait.java
@@ -1,5 +1,38 @@
 package com.Selenium.Practice;
 
-public class ExplicitWait {
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class ExplicitWait {
+	
+	WebDriver driver;
+	
+	@Test
+	public void explicitWait() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\Documents\\workspace-spring-tool-suite-4-4.14.1.RELEASE\\Selenium-Practice\\Selenium-Practice\\src\\main\\java\\com\\Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();https://www.amazon.in/
+		driver.get("https://www.amazon.in/");
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		clickOn(driver, driver.findElement(By.linkText("Account & Lists")),40 );
+		
+		
+	}
+	
+	public void clickOn(WebDriver driver, WebElement locator, int timeout) {
+		new WebDriverWait(driver, timeout).ignoring(StaleElementReferenceException.class).until(ExpectedConditions.elementToBeClickable(locator));
+		locator.click();
+	}
+	
 }
