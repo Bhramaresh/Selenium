6786a6b46cfc5b613815966643472268ee1b3a14
bhramaresh <bhramareshballa9646@gmail.com>
2022-11-24 23:35:48 +0530

    Practiced Authentication popup & Notifictions

--- src/main/java/com/Selenium/Practice/BrowserPOPUPS.java
+++ src/main/java/com/Selenium/Practice/BrowserPOPUPS.java
@@ -4,16 +4,21 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import com.gargoylesoftware.htmlunit.javascript.host.html.Option;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BrowserPOPUPS {
 	public static void main(String[] args) {
 		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();//Using this class we can disable notifications from the browser, based on the browser this class will be changed
+		options.addArguments("--disable-notifications");
+		WebDriver driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.get("https://www.apsrtconline.in/oprs-web/");
+		driver.get("https://www.redbus.in/");
 	}
 
 }
